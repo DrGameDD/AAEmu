011864f259efa426745858a864aa308e9b94bf41
jbirdsin <jbirdsin@gmail.com>
2019-08-01 19:15:18 -0600

    Updated DB
    Added Start script, updated db to reflect new code changes for npc loots

---
+++ AAEmu.Game/Data/DBUpdates/Update1.sql
@@ -0,0 +1,8 @@
+﻿BEGIN TRANSACTION;
+CREATE TABLE IF NOT EXISTS "loot_pack_dropping_npcs" (
+	"id"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	"npc_id"	integer NOT NULL,
+	"loot_pack_id"	integer NOT NULL,
+	"default_pack"	boolean NOT NULL DEFAULT 't'
+);
+COMMIT;
